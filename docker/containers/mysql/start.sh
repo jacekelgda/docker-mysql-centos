0aa6dfd16135b2e5acd263de5e5b617aa79f77ed
ulff <ulff.pl@gmail.com>
2015-03-30 12:45:11 +0200

    BET-81 creating second database for tests purposes

--- docker/containers/mysql/start.sh
+++ docker/containers/mysql/start.sh
@@ -11,6 +11,11 @@ if [ ! -d "$datadir/mysql" ] ; then
     mysql -e "CREATE USER '${DBUSER}'@'%' IDENTIFIED BY '${DBPASSWD}';" -p${ROOTPASSWD}
     mysql -e "GRANT ALL PRIVILEGES ON ${DBNAME}.* TO '${DBUSER}'@'%';" -p${ROOTPASSWD}
     mysql -e "USE ${DBNAME};SOURCE ${DBSOURCEPATH};" -p${ROOTPASSWD}
+
+    mysql -e "CREATE DATABASE ${DBTESTNAME}" -p${ROOTPASSWD}
+    mysql -e "CREATE USER '${DBUSER}'@'%' IDENTIFIED BY '${DBPASSWD}';" -p${ROOTPASSWD}
+    mysql -e "GRANT ALL PRIVILEGES ON ${DBTESTNAME}.* TO '${DBUSER}'@'%';" -p${ROOTPASSWD}
+    mysql -e "USE ${DBTESTNAME};SOURCE ${DBSOURCEPATH};" -p${ROOTPASSWD}
     killall mysqld
     sleep 10
 fi
